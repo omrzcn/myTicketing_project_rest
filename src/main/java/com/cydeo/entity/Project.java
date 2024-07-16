a214e1925d9277dfe83625b373d02cc6973562b4
omrzcn <omrzcnn@gmail.com>
2024-07-16 17:15:31 +0300

    entity

--- src/main/java/com/cydeo/entity/Project.java
+++ src/main/java/com/cydeo/entity/Project.java
@@ -5,7 +5,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.apache.catalina.User;
 import org.hibernate.annotations.Where;
 
 
---
+++ src/main/java/com/cydeo/entity/User.java
@@ -0,0 +1,39 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.Gender;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+//@Where(clause = "is_deleted=false")         // SELECT * FROM users WHERE id = 4 AND is_deleted = false;
+public class User extends BaseEntity {
+
+    private String firstName;
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String userName;
+
+    @Column(nullable = false)
+    private String passWord;
+
+    private boolean enabled;
+    private String phone;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+}
