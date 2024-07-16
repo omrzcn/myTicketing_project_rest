e68e0e847fe9aa312388396e0be3d69f10bdd8f7
omrzcn <omrzcnn@gmail.com>
2024-07-16 17:11:34 +0300

    entity

---
+++ src/main/java/com/cydeo/entity/Role.java
@@ -0,0 +1,22 @@
+package com.cydeo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role extends BaseEntity {
+
+    private String description;
+
+}
